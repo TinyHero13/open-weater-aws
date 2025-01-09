1bf82248c902b193f1c0f9cab9f2cf6042e466b2
TinyHero13 <yasmimcharles@protonmail.com>
2025-01-09 16:12:11 -0300

    feat: adicionando mais cidades

--- src/extract.py
+++ src/extract.py
@@ -7,13 +7,17 @@ load_dotenv()
 def extract():
     API_KEY = os.getenv('OPENWEATHER_API_KEY') 
 
-    city = ['Rio de Janeiro', 'Sao Paulo']
-    base_url = "http://api.openweathermap.org/data/2.5/weather"
-    params = {
-        "q": city,
-        "appid": API_KEY,
-        "units": "metric"
-    }
-    response = requests.get(base_url, params=params).json()
-    
-    return response
+    cities = ['Rio de Janeiro', 'Belford Roxo', 'Nova Iguaçu']
+    temp_list = []
+
+    for city in cities:
+        base_url = "http://api.openweathermap.org/data/2.5/weather"
+        params = {
+            "q": city,
+            "appid": API_KEY,
+            "units": "metric"
+        }
+        response = requests.get(base_url, params=params).json()
+        temp_list.append(response)
+
+    return temp_list
